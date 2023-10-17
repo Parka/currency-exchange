996045fe65b8c64d6a5b2a2ba9fc0873e4120aae
Parka <franciscocasanovaferro@gmail.com>
2023-10-17 18:14:43 -0300

    feat: add currency change

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,88 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import freecurrencyapi from "./api"
+import { Currencies } from "@everapi/freecurrencyapi-js"
 
 function App() {
+  const [currencies, setCurrencies] = useState<Currencies>({})
   const [currency1, setCurrency1] = useState("USD")
   const [currency2, setCurrency2] = useState("MXN")
-  const [loading, setLoading] = useState(true)
+  const [loadingCurrencies, setLoadingCurrencies] = useState(true)
+  const [loadingRates, setLoadingRates] = useState(true)
+  const loading = loadingCurrencies || loadingRates
   const [rate, setRate] = useState(0)
   const [currVal1, setCurrVal1] = useState("")
   const [currVal2, setCurrVal2] = useState("")
+  const flagUpdate1 = useRef(false)
+  const flagUpdate2 = useRef(false)
 
+  /// FETCH CURRENCIES
+  useEffect(() => {
+    const fetchCurrencies = async () => {
+      setLoadingCurrencies(true)
+      const { data } = await freecurrencyapi.currencies()
+      setCurrencies(data)
+      setLoadingCurrencies(false)
+    }
+    fetchCurrencies()
+  }, [])
+
+  /// FETCH RATES
   useEffect(() => {
     const fetchRate = async () => {
+      setLoadingRates(true)
       const { data } = await freecurrencyapi.latest({
         base_currency: currency1,
         currencies: currency2,
       })
       setRate(data[currency2])
-      setLoading(false)
+      setLoadingRates(false)
     }
     fetchRate()
   }, [currency1, currency2])
 
-  type OnChange = React.InputHTMLAttributes<HTMLInputElement>["onChange"]
-  const onChange1: OnChange = (e) => {
+  useEffect(() => {
+    if (!rate) return
+    if (flagUpdate1.current) {
+      flagUpdate1.current = false
+      setCurrVal1(String(Number(currVal2) / rate || ""))
+    }
+    if (flagUpdate2.current) {
+      flagUpdate2.current = false
+      setCurrVal2(String(Number(currVal1) * rate || ""))
+    }
+  }, [currVal1, currVal2, rate])
+
+  // EVENT HANDLERS INPUTS
+  type OnChangeInput = React.InputHTMLAttributes<HTMLInputElement>["onChange"]
+
+  const onChange1: OnChangeInput = (e) => {
     const value = Number(e.target.value)
     if (isNaN(value)) return
     setCurrVal1(e.target.value)
     setCurrVal2(String(value * rate || ""))
   }
 
-  const onChange2: OnChange = (e) => {
+  const onChange2: OnChangeInput = (e) => {
     const value = Number(e.target.value)
     if (isNaN(value)) return
     setCurrVal2(e.target.value)
     setCurrVal1(String(value / rate || ""))
+  }
+
+  // EVENT HANDLERS SELECTS
+  type onChangeSelect = React.SelectHTMLAttributes<HTMLSelectElement>["onChange"]
+
+  const onChangeCurrency1: onChangeSelect = (e) => {
+    flagUpdate1.current = true
+    setRate(0)
+    setCurrVal1("")
+    setCurrency1(e.target.value)
+  }
+  const onChangeCurrency2: onChangeSelect = (e) => {
+    flagUpdate2.current = true
+    setRate(0)
+    setCurrVal2("")
+    setCurrency2(e.target.value)
   }
 
   return (
@@ -53,9 +102,11 @@ function App() {
             onChange={onChange1}
             disabled={loading}
           />
-          <label htmlFor="currency1" className="w-10">
-            {currency1}
-          </label>
+          <select value={currency1} onChange={onChangeCurrency1} disabled={loading}>
+            {Object.values(currencies).map(({ code }) => (
+              <option key={code}>{code}</option>
+            ))}
+          </select>
         </div>
         <div className="flex">
           <input
@@ -68,9 +119,11 @@ function App() {
             onChange={onChange2}
             disabled={loading}
           />
-          <label htmlFor="currency2" className="w-10">
-            {currency2}
-          </label>
+          <select value={currency2} onChange={onChangeCurrency2} disabled={loading}>
+            {Object.values(currencies).map(({ code }) => (
+              <option key={code}>{code}</option>
+            ))}
+          </select>
         </div>
       </form>
     </>
--- src/freecurrencyapi.d.ts
+++ src/freecurrencyapi.d.ts
@@ -1,6 +1,18 @@
 declare module "@everapi/freecurrencyapi-js" {
+  export interface Currency {
+    symbol: string
+    name: string
+    symbol_native: string
+    decimal_digits: number
+    rounding: number
+    code: string
+    name_plural: string
+  }
+  export interface Currencies extends Record<string, Currency> {}
+
   export default class Freecurrencyapi {
     constructor(key: string)
+    currencies(): Promise<{ data: Currencies }>
     latest(config: { base_currency: string; currencies: string }): Promise<{
       data: Record<string, number>
     }>
